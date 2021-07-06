cdc03cf2f68477f293435c2a717fecf12818f928
Pulkit Aggarwal <pulkit@student.unimelb.edu.au>
2021-07-06 22:11:16 +0530

    scraper-fixed

--- scrapers/app.py
+++ scrapers/app.py
@@ -8,6 +8,7 @@ import numpy as np
 import json
 
 
+# Unimelb Handbook Link
 
 URL = "https://handbook.unimelb.edu.au/search?types%5B%5D=subject&year=2021&subject_level_type%5B%5D=all&study_periods%5B%5D=all&area_of_study%5B%5D=all&org_unit%5B%5D=all&campus_and_attendance_mode%5B%5D=all&page=1&sort=_score%7Cdesc"
 z = 1
@@ -23,7 +24,6 @@ with open('data.json', 'w', newline='') as file:
         r = requests.get(URL)
         soup = BeautifulSoup(r.content, 'html.parser') 
         z = z + 1
-        # Dict to be converted to df
         majors = {}
         links = soup.findAll('div',class_='search-result-item__name')
 
@@ -33,13 +33,11 @@ with open('data.json', 'w', newline='') as file:
             c = i.text
             subject_name = c[:-9]
             subject_code = c[-9:]
-            #print(z)
             a = " { \"subjectName\": \"" + subject_name + "\", \"subjectCode\": \"" + subject_code + "\" }"
             d = json.loads(a)
             subjects.append(d)
             
-            #print(subjects)
-        print(z)
+            
 
         URL = "https://handbook.unimelb.edu.au/search?types%5B%5D=subject&year=2021&subject_level_type%5B%5D=all&study_periods%5B%5D=all&area_of_study%5B%5D=all&org_unit%5B%5D=all&campus_and_attendance_mode%5B%5D=all&page=" + str(z) + "&sort=_score%7Cdesc"
 
