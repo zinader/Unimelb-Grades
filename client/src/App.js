fc3967e82d5c08706cfb3d309d87d7d1aefdb33d
Pulkit Aggarwal <pulkit@student.unimelb.edu.au>
2021-07-06 20:58:01 +0530

    main-component-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import MainComponent from "./components/MainComponent";
 
 function App() {
-  return <div className="App">Pulkit</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" exact component={MainComponent} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/MainComponent.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+function MainComponent() {
+  return (
+    <div>
+      <Header />
+      <div className="subheading">
+        <h3>
+          Here you can find an idea of the subject scores people have attained
+          in the past semesters. Hope this helps :)
+        </h3>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default MainComponent;
