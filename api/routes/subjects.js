0cf712a75940df7bf0f505c03ade65f8310729fb
Pulkit Aggarwal <pulkit@student.unimelb.edu.au>
2021-07-06 22:18:05 +0530

    all-routes

--- api/routes/subjects.js
+++ api/routes/subjects.js
@@ -2,6 +2,23 @@ import express from "express";
 const router = express.Router();
 import Subject from "../models/subjects_model.js";
 
+router.route("/").get((req, res) => {
+  Subject.find()
+    .sort({ scores: -1 })
+    .then((result) =>
+      res.json({
+        success: true,
+        data: result,
+      })
+    )
+    .catch((err) =>
+      res.json({
+        success: false,
+        error: err,
+      })
+    );
+});
+
 // Get top 20 subjects
 
 router.route("/top").get((req, res) => {
