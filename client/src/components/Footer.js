0c39b3a8f9719a10953f7c39fbae438687cb7df6
Pulkit Aggarwal <pulkit@student.unimelb.edu.au>
2021-07-06 20:54:34 +0530

    footer-done

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        Created with <span>&hearts;</span> by{" "}
+        <a target="_blank" rel="noreferrer" href="https://github.com/zinader">
+          Pulkit Aggarwal
+        </a>{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
