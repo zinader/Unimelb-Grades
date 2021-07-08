d6bad2b599c401fdbbbe96708829a0add1f5e8dd
Pulkit Aggarwal <pulkit@student.unimelb.edu.au>
2021-07-08 05:36:26 +0530

    data-updated

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -14,6 +14,20 @@ function MainComponent() {
           Here you can find an idea of the subject scores people have attained
           in the past semesters. Hope this helps :)
         </h3>
+        <h3>
+          {" "}
+          Mostly all the subjects are in the list but if you can find a subject
+          which is not, please fill this{" "}
+          <a
+            href="https://forms.gle/Co1wpzbGnNoX7dC48"
+            className="missing-form"
+            target="_blank"
+            rel="noreferrer"
+          >
+            form
+          </a>{" "}
+          up and I'll add it asap.
+        </h3>
       </div>
 
       <SubjectComponent />
