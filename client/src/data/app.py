fb14f9f35ea206139e92ba902786e031c8740a06
Pulkit Aggarwal <pulkit@student.unimelb.edu.au>
2021-07-10 02:22:40 +0530

    mongodb

---
+++ client/src/data/app.py
@@ -0,0 +1,20 @@
+import json
+
+k = []
+count = 0
+        
+with open('data.json', 'w', newline='') as file:
+    with open('data.txt', 'r') as f:
+        for line in f.readlines():        
+            d = json.loads(line)
+            subject_name = d['subjectName']
+            subject_code = d['subjectCode']
+            
+            a = '{ ' + '"subjectName"' + ': \"' + subject_name + '\" , ' + '"subjectCode"' + ': \"' + subject_code + '\" }'
+            #print(a)
+            count +=1
+            print(count)
+            k.append(json.loads(a))
+    json.dump(k, file, indent=4)
+        
+
